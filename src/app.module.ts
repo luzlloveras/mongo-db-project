999c301de74a179106223b5782b1e236a7991d2e
luzlloveras <luzlloveras7@gmail.com>
2023-11-10 20:08:08 -0300

    correct conexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { ArtistsModule } from './artists/artists.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/luzlloveras.artist'),
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/luzlloveras'),
   ArtistsModule,
 ],
 controllers: [AppController],
