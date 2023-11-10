5eae433672031f8105b15c649327f922e61debd0
luzlloveras <luzlloveras7@gmail.com>
2023-11-10 19:53:20 -0300

    add module artists

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+//Modules
+import { ArtistsModule } from './artists/artists.module';
+
+
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost:27017/luzlloveras.artist'),
   ArtistsModule,
