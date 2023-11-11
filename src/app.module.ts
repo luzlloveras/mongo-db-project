ac656bd4535f8e56ab036d029003dfc2c33c7543
luzlloveras <luzlloveras7@gmail.com>
2023-11-11 11:42:03 -0300

    correccion ruta localhost

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { ArtistsModule } from './artists/artists.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/luzlloveras'),
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/luzlloveras'),
   ArtistsModule,
 ],
 controllers: [AppController],
